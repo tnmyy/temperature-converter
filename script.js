cbba86c1fc6ac447be57ffe48149f9286fc3e4a5
Tanmay Upreti <iamtanmayupreti@gmail.com>
2023-06-23 16:21:53 +0530

    Minor Changes

--- script.js
+++ script.js
@@ -8,42 +8,42 @@ let kelvin = document.getElementById("kelvin");
 
 // Celsius to Fahrenheit Function
 
-const celToFah = () => {
+celsius.addEventListener("input", () => {
   let c2f = (parseFloat(celsius.value) * 9) / 5 + 32;
   fahrenheit.value = parseFloat(c2f.toFixed(2));
-};
+});
 
 // Celsius To Kelvin Function
 
-const celToKel = () => {
+celsius.addEventListener("input", () => {
   let c2k = parseFloat(celsius.value) + 273.15;
   kelvin.value = parseFloat(c2k);
-};
+});
 
 // Fahrenheit to Celsius Function
 
-const fahToCel = () => {
+fahrenheit.addEventListener("input", () => {
   let f2c = ((parseFloat(fahrenheit.value) - 32) * 5) / 9;
   celsius.value = parseFloat(f2c.toFixed(2));
-};
+});
 
 // Fahrenheit to Kelvin Function
 
-const fahToKel = () => {
+fahrenheit.addEventListener("input", () => {
   let f2k = ((parseFloat(fahrenheit.value) - 32) * 5) / 9 + 273.15;
   kelvin.value = parseFloat(f2k.toFixed(2));
-};
+});
 
 // Kelvin to Celsius Function
 
-const kelToCel = () => {
+kelvin.addEventListener("input", () => {
   let k2c = parseFloat(kelvin.value) - 273.15;
   celsius.value = parseFloat(k2c);
-};
+});
 
 // Kelvin to Fahrenheit Function
 
-const kelToFah = () => {
+kelvin.addEventListener("input", () => {
   let k2f = ((parseFloat(kelvin.value) - 273.15) * 9) / 5 + 32;
   fahrenheit.value = parseFloat(k2f.toFixed(2));
-};
+});
